923b1f64b594a3fd37bb8eea34b79b6677c65d85
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-02 09:20:17 -0300

    node_modules atualizado

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//Importante que seja dado o comando "yarn" no mesmo diretório de package.json, senão node_modules/ não existirá 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './ComponenteMain/App';
 import * as serviceWorker from './serviceWorker';
