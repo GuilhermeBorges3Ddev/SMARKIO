1a63e8e4fba269bdcd8b5ae3d8c80355ab66aa77
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-02 10:25:30 -0300

    Lado direito terminado

--- front-end/src/Componentes/ClimaInfo.js
+++ front-end/src/Componentes/ClimaInfo.js
@@ -7,31 +7,39 @@ import React from 'react';
 const ClimaInfo = props => (  
   <div className="weather_info">
     {props.temperatura && 
-      <p className="weather__key">Cidade pesquisada: <span className="weather__value">{props.cidade}</span> </p>
+      <p className="weather__key">Cidade pesquisada: 
+        <span className="weather__value"> {props.cidade}</span> 
+      </p>
     }
 
     {props.pais && 
-      <p className="weather__key">Sigla do país da cidade buscada: <span className="weather__value">{props.pais}</span> </p>
+      <p className="weather__key">Sigla do país da cidade buscada: 
+        <span className="weather__value"> {props.pais}</span> 
+      </p>
     }
         
     {props.temperatura && 
-      <p className="weather__key">Temperatura: <span className="weather__value">{props.temperatura}º</span> </p>
+      <p className="weather__key">Temperatura: 
+        <span className="weather__value"> {props.temperatura}º</span> 
+      </p>
     }
         
     {props.umidade && 
-      <p className="weather__key">Umidade: <span className="weather__value">{props.umidade}%</span> </p>
+      <p className="weather__key">Umidade: 
+        <span className="weather__value"> {props.umidade}%</span> 
+      </p>
     }
         
     {props.descricao && 
-      <p className="weather__key">Condições: <span className="weather__value">{props.descricao}</span> </p>
+      <p className="weather__key">Condições: 
+        <span className="weather__value"> {props.descricao}</span> 
+      </p>
     }
 
     {props.latitude &&
-      <p className="weather__key">Latitude: <span className="weather__value">{props.latitude}</span> </p>
-    }
-
-    {props.longitude &&
-      <p className="weather__key">Longitude: <span className="weather__value">{props.longitude}</span> </p>
+      <p className="weather__key">Latitude e Longitude: 
+        <span className="weather__value"> {props.latitude}lat / {props.longitude}lng </span> 
+      </p>
     }
 
     {props.error &&
