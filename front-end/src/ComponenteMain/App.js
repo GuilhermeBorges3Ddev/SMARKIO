acbe11df0d9c43e87bfb85cf38d80ea6baad3a10
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2019-11-02 07:27:38 -0300

    Componente App.js já passa o resultado da requisição por meio de props para o componente ClimaInfo.js

--- front-end/src/ComponenteMain/App.js
+++ front-end/src/ComponenteMain/App.js
@@ -10,13 +10,36 @@ const API_KEY = "81101292e91a941f627ea27ec02cf4bd";
 
 class App extends React.Component{
 
+  //Pegando as alterações nos dados de busca por busca, armazenamento em estados(states), inicialmente "undefined"
+  state = {
+    temperatura: undefined,
+    cidade: undefined,
+    pais: undefined,
+    umidade: undefined,
+    descricao: undefined,
+    error: undefined
+  }
+
   getData = async (e) => {
+    
     e.preventDefault();//Previne que a requisição disparada não resete pelo reload do React
+    
     const cidade = e.target.elements.cidade.value;
     const pais = e.target.elements.pais.value;
     const api_req = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cidade},${pais}&appid=${API_KEY}&units=metric`); 
     const data = await api_req.json();
+    
     console.log(data);
+
+    this.setState({
+      temperatura: data.main.temp,
+      cidade: data.name,
+      pais: data.sys.country,
+      umidade: data.main.humidity,
+      descricao: data.weather[0].description,
+      error: ""
+    });
+
   }
 
   render(){
@@ -24,7 +47,14 @@ class App extends React.Component{
       <div className="App">
         <Titulos/>
         <Formularios getData={this.getData}/>
-        <ClimaInfo/>
+        <ClimaInfo
+          temperatura={this.state.temperatura}
+          cidade={this.state.cidade}
+          pais={this.state.pais}
+          umidade={this.state.umidade}
+          descricao={this.state.descricao}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- front-end/src/Componentes/ClimaInfo.js
+++ front-end/src/Componentes/ClimaInfo.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-function ClimaInfo() {
+class ClimaInfo extends React.Component {
+  render(){
     return (
       <div>
-        <p>Informações climáticas</p>
+        {this.props.temperatura && 
+          <p>Cidade pesquisada: {this.props.cidade}</p>
+        }
+
+        {this.props.pais && 
+          <p>Sigla do país da cidade buscada: {this.props.pais}</p>
+        }
+        
+        {this.props.temperatura && 
+          <p>Temperatura: { this.props.temperatura }</p>
+        }
+        
+        {this.props.umidade && 
+          <p>Umidade: {this.props.umidade}</p>
+        }
+        
+        {this.props.descricao && 
+          <p>Condições: { this.props.descricao }</p>
+        }
       </div>
     );
   }
+};
   
-  export default ClimaInfo;
+export default ClimaInfo;
